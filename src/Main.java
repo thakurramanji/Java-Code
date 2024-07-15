a8515afdc984d2c908c6afb42ae3a1aa6bb5aff5
Ramanji Kumar Thakur <NH2883@nslhub.in>
2024-07-15 17:04:21 +0530

    interface with lambda

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,41 @@
 
 // enum details
 
-enum Laptop {
-    Mac(100000), Dell(44000), Lenovo(59000), Asus(50000);
-    int price;
-    Laptop(int price) {
-        this.price = price;
-    }
-    public int getPrice() {
-        return price;
-    }
+interface A {
+    void show(int i);
 }
 
 public class Main {
-
     public static void main(String[] args) {
 
-        for(Laptop l: Laptop.values()) {
-            System.out.println(l.name() + " : "+ l.getPrice());
-        }
+        A obj = (int i) -> System.out.println("in show "+ i);
+
+        obj.show(5);
+
     }
 }
+
+
+//enum Laptop {
+//    Mac(100000), Dell(44000), Lenovo(59000), Asus(50000);
+//    int price;
+//    Laptop(int price) {
+//        this.price = price;
+//    }
+//    public int getPrice() {
+//        return price;
+//    }
+//}
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        for(Laptop l: Laptop.values()) {
+//            System.out.println(l.name() + " : "+ l.getPrice());
+//        }
+//    }
+//}
 // Abstract keyword
 //
 //abstract class A {
