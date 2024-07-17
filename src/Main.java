51538ac7efcc3b47659b9035eed3001938dfa0b3
Ramanji Kumar Thakur <NH2883@nslhub.in>
2024-07-17 11:10:22 +0530

    race condition added

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,84 @@
 
-// our own exception while handling exception
-class RamaException extends Exception {
-    public RamaException(String s) {
-        super(s);
+
+// Race condition resolve with synchronized
+
+class Cnt {
+    int val;
+
+    public synchronized void inc() {
+        val++;
     }
 }
-public class Main {
-    public static void main(String[] args) {
 
-        int i=30;
-        int j=0;
+class Main {
+    public static void main(String[] args) {
+        Cnt c = new Cnt();
+
+        Runnable obj1 = () -> {
+            for (int i = 0; i < 10000; i++) {
+                c.inc();
+            }
+        };
+
+        Runnable obj2 = () -> {
+            for (int i = 0; i < 10000; i++) {
+                c.inc();
+            }
+        };
+
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
+
+        t1.start();
+        t2.start();
 
         try {
-            j = 18/i;
-            if(j==0) {
-                throw new RamaException("Looking Good!!");
-            }
-
-        }
-        catch (RamaException e) {
-            System.out.println("Can not divided by zero--");
-        }
-        catch (Exception e) {
-            System.out.println("someting went wrong...");
+            t1.join();
+            t2.join(); // Wait for both threads to finish
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("Bye...");
+        System.out.println(c.val); // Now the expected output is 20000
     }
 }
+
+
+
+
+
+
+
+
+// our own exception while handling exception
+//class RamaException extends Exception {
+//    public RamaException(String s) {
+//        super(s);
+//    }
+//}
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        int i=30;
+//        int j=0;
+//
+//        try {
+//            j = 18/i;
+//            if(j==0) {
+//                throw new RamaException("Looking Good!!");
+//            }
+//
+//        }
+//        catch (RamaException e) {
+//            System.out.println("Can not divided by zero--");
+//        }
+//        catch (Exception e) {
+//            System.out.println("someting went wrong...");
+//        }
+//
+//        System.out.println("Bye...");
+//    }
+//}
 //public class Main {
 //    public static void main(String[] args) {
 //
